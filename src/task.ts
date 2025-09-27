01d867d5149708e3267df060cc631776d4d5b594
Kos <kostyaveselovsky@gmail.com>
2025-09-27 22:34:22 +0300

    feat: improve output of the task function

--- src/task.ts
+++ src/task.ts
@@ -1,9 +1,15 @@
 import {Result} from "./modules/apt";
 
 export async function task(name: string, taskObject: Promise<Result>) {
+    process.stdout.write('[ ] ' + name);
+
     const result = await taskObject;
 
-    console.log(name);
+    if (result.failed) {
+        process.stdout.write('\r\x1b[1;31m[ ] ' + name + '\x1b[0m\n');
+    } else {
+        process.stdout.write('\r\x1b[1;32m[x] ' + name + '\x1b[0m\n');
+    }
 
     return result;
 }
