999d9419fc0dbca9544bf2a3add2496cd9240214
Kos <kostyaveselovsky@gmail.com>
2025-10-02 01:32:35 +0300

    feat(file): add idempotency support to the upload method

--- src/modules/file.test.ts
+++ src/modules/file.test.ts
@@ -11,7 +11,40 @@ it('should successfully upload file', async () => {
         remotePath: '/tmp/.gitignore',
     });
 
+    expect(true).toBe(result.changed);
     expect(false).toBe(result.failed);
+}, 60 * 1000);
+
+it('should replace file if it already exists but has been modified', async () => {
+    const host = createHost();
+    await host.command({command: 'echo 1 > /tmp/modified_file'});
+
+    const result = await file.upload(host, {
+        localPath: __dirname + '/../../.gitignore',
+        remotePath: '/tmp/modified_file',
+    });
+
+    expect(true).toBe(result.changed);
+    expect(false).toBe(result.failed);
+});
+
+it('should return a result with the changed property set to false if the file already uploaded', async () => {
+    const host = createHost();
+    const result = await file.upload(host, {
+        localPath: __dirname + '/../../.gitignore',
+        remotePath: '/tmp/already_uploaded_file',
+    });
+
+    expect(true).toBe(result.changed);
+    expect(false).toBe(result.failed);
+
+    const result2 = await file.upload(host, {
+        localPath: __dirname + '/../../.gitignore',
+        remotePath: '/tmp/already_uploaded_file',
+    });
+
+    expect(false).toBe(result2.changed);
+    expect(false).toBe(result2.failed);
 }, 60 * 1000);
 
 it('should return an error when uploading a non-existent file', async () => {
--- src/modules/file.ts
+++ src/modules/file.ts
@@ -1,6 +1,7 @@
 import {Host} from "../hosts/host";
 import * as fs from "node:fs";
 import * as path from "node:path";
+import * as crypto from "node:crypto";
 import {TaskResult} from "../task";
 
 export interface Options {
@@ -18,14 +19,26 @@ export const file = {
             };
         }
 
-        const result = await host.command({command: 'test -d ' + path.dirname(options.remotePath)});
-
-        if (result.code !== 0) {
+        const testDirectoryResult = await host.command({command: 'test -d ' + path.dirname(options.remotePath)});
+        if (testDirectoryResult.code !== 0) {
             return {
                 changed: false,
                 failed: true,
                 error: 'remote directory does not exist',
             };
+        }
+
+        const testFileResult = await host.command({command: 'test -f ' + options.remotePath});
+        if (testFileResult.code === 0) {
+            const calcHashResult = await host.command({command: 'sha256sum ' + options.remotePath});
+            const hash = calcHashResult.stdout.trim().split(/ +/)[0];
+
+            if (calcHashResult.code === 0 && hash === await sha256(options.localPath)) {
+                return {
+                    changed: false,
+                    failed: false,
+                };
+            }
         }
 
         await host.uploadFile(options);
@@ -36,3 +49,16 @@ export const file = {
         };
     },
 };
+
+async function sha256(filename: string): Promise<string> {
+    const stream = fs.createReadStream(filename);
+
+    let data = '';
+    for await (const chunk of stream) {
+        data += chunk;
+    }
+
+    return crypto.createHash('sha256')
+        .update(data)
+        .digest('hex');
+}
