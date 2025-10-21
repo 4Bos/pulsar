e17ae8ca9a8a1679131172084f092999bb3a9ca0
Kos <kostyaveselovsky@gmail.com>
2025-10-21 08:17:27 +0300

    feat(file): implement createDirectory method

--- src/modules/file.test.ts
+++ src/modules/file.test.ts
@@ -1,4 +1,4 @@
-import {expect, it} from "vitest";
+import {expect, it, describe} from "vitest";
 import {prepareHost} from "../testing";
 import {file} from "./file";
 
@@ -68,3 +68,156 @@ it('should return an error when uploading to a non-existent directory', async ()
     expect(true).toBe(result.failed);
     expect('remote directory does not exist').toBe(result.error);
 }, 60 * 1000);
+
+describe('createDirectory method', () => {
+    it('should successfully create directory', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, '/test1');
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'test -d /test1'});
+
+        expect(testResult.code).toBe(0);
+    }, 60 * 1000);
+
+    it('should return an error if we try to create a directory without the recursion flag and inside a non-existent directory', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, '/non-existent/test1');
+
+        expect(result.changed).toBe(false);
+        expect(result.failed).toBe(true);
+        expect(result.error).toBe('parent directory does not exist');
+    }, 60 * 1000);
+
+    it('should recursively create all non-existent directories', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, {
+            path: '/d1/d2/d3',
+            recursive: true,
+        });
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'test -d /d1/d2/d3'});
+
+        expect(testResult.code).toBe(0);
+    }, 60 * 1000);
+
+    it('should create directory with specified mode', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, {
+            path: '/dir10',
+            mode: '0137',
+        });
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'stat -c "%a" /dir10'});
+
+        expect(testResult.code).toBe(0);
+        expect(testResult.stdout.trim()).toBe('137');
+    }, 60 * 1000);
+
+    it('should create directory with specified group and owner', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, {
+            path: '/dir12',
+            owner: '6000',
+            group: '5000',
+        });
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'stat -c "%u %g" /dir12'});
+
+        expect(testResult.code).toBe(0);
+        expect(testResult.stdout.trim()).toBe('6000 5000');
+    }, 60 * 1000);
+
+    it('should return an error if we try to create a directory with non-existent owner', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, {
+            path: '/dir24',
+            owner: 'unknownuser',
+        });
+
+        expect(result.changed).toBe(false);
+        expect(result.failed).toBe(true);
+        expect(result.error).toBe('the specified owner does not exist');
+    }, 60 * 1000);
+
+    it('should return an error if we try to create a directory with non-existent group', async () => {
+        const host = createHost();
+
+        const result = await file.createDirectory(host, {
+            path: '/dir24',
+            group: 'unknowgroup',
+        });
+
+        expect(result.changed).toBe(false);
+        expect(result.failed).toBe(true);
+        expect(result.error).toBe('the specified group does not exist');
+    }, 60 * 1000);
+
+    it('should return an error if path contains an entry that is not a directory', async () => {
+        const host = createHost();
+        await host.command({command: 'mkdir /dir500 && touch /dir500/file'});
+
+        const result = await file.createDirectory(host, {
+            path: '/dir500/file/dir',
+            recursive: true,
+        });
+
+        expect(result.changed).toBe(false);
+        expect(result.failed).toBe(true);
+        expect(result.error).toBe('path contains an entry that is not a directory');
+    }, 60 * 1000);
+
+    it('should change owner and group of an existing directory', async () => {
+        const host = createHost();
+        await host.command({command: 'mkdir /dir777 && chown 5000:6000 /dir777'});
+
+        const result = await file.createDirectory(host, {
+            path: '/dir777',
+            owner: '7000',
+            group: '8000',
+        });
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'stat -c "%u %g" /dir777'});
+
+        expect(testResult.code).toBe(0);
+        expect(testResult.stdout.trim()).toBe('7000 8000');
+    }, 60 * 1000);
+
+    it('should change mode of an existing directory', async () => {
+        const host = createHost();
+        await host.command({command: 'mkdir /dir888 && chmod 0777 /dir888'});
+
+        const result = await file.createDirectory(host, {
+            path: '/dir888',
+            mode: '1444',
+        });
+
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+
+        const testResult = await host.command({command: 'stat -c "%a" /dir888'});
+
+        expect(testResult.code).toBe(0);
+        expect(testResult.stdout.trim()).toBe('1444');
+    }, 60 * 1000);
+});
--- src/modules/file.ts
+++ src/modules/file.ts
@@ -9,6 +9,14 @@ export interface Options {
     remotePath: string;
 }
 
+export interface CreateDirectoryOptions {
+    path: string;
+    mode?: string;
+    group?: string;
+    owner?: string;
+    recursive?: boolean;
+}
+
 export const file = {
     upload: async (host: Host, options: Options): Promise<TaskResult> => {
         if (!fs.existsSync(options.localPath)) {
@@ -47,6 +55,156 @@ export const file = {
             changed: true,
             failed: false,
         };
+    },
+
+    createDirectory: async (host: Host, optionsOrPath: CreateDirectoryOptions|string) => {
+        const options = typeof optionsOrPath === 'string' ? {path: optionsOrPath} : optionsOrPath;
+
+        if (!options.recursive) {
+            const parentDir = path.dirname(options.path);
+
+            if (parentDir) {
+                const testDirExistResult = await host.command({command: 'test -d ' + parentDir});
+                if (testDirExistResult.code !== 0) {
+                    return {
+                        changed: false,
+                        failed: true,
+                        error: 'parent directory does not exist',
+                    };
+                }
+            }
+        } else {
+            let parentDir = path.dirname(options.path);
+
+            while (parentDir && parentDir !== '/') {
+                const testFileResult = await host.command({command: 'test -e ' + parentDir});
+                if (testFileResult.code === 0) {
+                    
+                    const testDirExistResult = await host.command({command: 'test -d ' + parentDir});
+                    if (testDirExistResult.code !== 0) {
+                        return {
+                            changed: false,
+                            failed: true,
+                            error: 'path contains an entry that is not a directory',
+                        };
+                    }
+                }
+
+                parentDir = path.dirname(parentDir);
+            }
+        }
+
+        let owner = options.owner ? options.owner : '';
+        let group = options.group ? ':' + options.group : '';
+
+        if (owner && /^\D+$/.test(owner)) {
+            const result = await host.command({command: 'id ' + owner});
+            if (result.code !== 0) {
+                return {
+                    changed: false,
+                    failed: true,
+                    error: 'the specified owner does not exist',
+                };
+            }
+        }
+
+        if (group && /^\D+$/.test(group)) {
+            const result = await host.command({command: 'getent group ' + group});
+            if (result.code !== 0) {
+                return {
+                    changed: false,
+                    failed: true,
+                    error: 'the specified group does not exist',
+                };
+            }
+        }
+
+        const testFileResult = await host.command({command: 'test -e ' + options.path});
+        if (testFileResult.code === 0) {
+            const testFileResult = await host.command({command: 'test -d ' + options.path});
+            if (testFileResult.code !== 0) {
+                return {
+                    changed: false,
+                    failed: true,
+                    error: 'the file at the specified path already exists and is not a directory',
+                };
+            }
+
+            const statResult = await host.command({command: 'stat -c "%U %u %G %g %a" ' + options.path});
+            if (statResult.code !== 0) {
+                return {
+                    changed: false,
+                    failed: true,
+                    error: 'unable to retrieve information about the specified directory',
+                };
+            }
+
+            const parts = statResult.stdout.trim().split(' ');
+            const currentMode = (parts[4].length === 3 ? '0' : '') + parts[4];
+            const ownerChanged = options.owner && (/^\d+$/.test(options.owner)
+                ? options.owner !== parts[1]
+                : options.owner !== parts[0]);
+            const groupChanged = options.group && (/^\d+$/.test(options.group)
+                ? options.group !== parts[3]
+                : options.group !== parts[2]
+            );
+            const modeChanged = options.mode && options.mode !== currentMode;
+
+            if (ownerChanged || groupChanged) {
+                const chownResult = await host.command({command: 'chown ' + owner + group + ' ' + options.path});
+                if (chownResult.code !== 0) {
+                    return {
+                        changed: true,
+                        failed: true,
+                        error: 'unable to change directory owner or group',
+                    };
+                }
+            }
+
+            if (modeChanged) {
+                const chmodResult = await host.command({command: 'chmod ' + options.mode + ' ' + options.path});
+                if (chmodResult.code !== 0) {
+                    return {
+                        changed: true,
+                        failed: true,
+                        error: 'unable to change directory mode',
+                    };
+                }
+            }
+
+            return {
+                changed: ownerChanged || groupChanged || modeChanged,
+                failed: false,
+            };
+        } else {
+            let mode = options.mode ? '-m ' + options.mode + ' ' : '';
+            let recursive = options.recursive ? '-p ' : '';
+
+            const createDirResult = await host.command({command: 'mkdir ' + mode + recursive + options.path});
+            if (createDirResult.code !== 0) {
+                return {
+                    changed: false,
+                    failed: true,
+                    error: 'unable to create directory',
+                };
+            }
+
+            if (owner || group) {
+                const chownResult = await host.command({command: 'chown ' + owner + group + ' ' + options.path});
+                if (chownResult.code !== 0) {
+                    return {
+                        changed: true,
+                        failed: true,
+                        error: 'unable to change directory owner or group',
+                    };
+                }
+            }
+
+            return {
+                changed: true,
+                failed: false,
+            };
+        }
     },
 };
 
