39f4fe27b5c8e7cb821b7bcd06cab500a7b14d56
Kos <kostyaveselovsky@gmail.com>
2025-10-23 19:14:16 +0300

    test(file): move upload file tests to a separate describe block

--- src/modules/file.test.ts
+++ src/modules/file.test.ts
@@ -4,108 +4,110 @@ import {file} from "./file";
 
 const createHost = prepareHost('4bos/pulsar:ssh-0.1');
 
-it('should successfully upload file', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        localPath: __dirname + '/../../.gitignore',
-        remotePath: '/tmp/.gitignore',
+describe('upload method' , () => {
+    it('should successfully upload file', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            localPath: __dirname + '/../../.gitignore',
+            remotePath: '/tmp/.gitignore',
+        });
+    
+        expect(true).toBe(result.changed);
+        expect(false).toBe(result.failed);
+    }, 60 * 1000);
+    
+    it('should upload file content from string', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            content: 'Hello world!',
+            remotePath: '/tmp/test-file3',
+        });
+    
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+    
+        const catResult = await host.command({
+            command: 'cat /tmp/test-file3',
+        });
+    
+        expect(catResult.code).toBe(0);
+        expect(catResult.stdout).toBe('Hello world!');
+    
+    }, 60 * 1000);
+    
+    it('should replace file if it already exists but has been modified', async () => {
+        const host = createHost();
+        await host.command({command: 'echo 1 > /tmp/modified_file'});
+    
+        const result = await file.upload(host, {
+            localPath: __dirname + '/../../.gitignore',
+            remotePath: '/tmp/modified_file',
+        });
+    
+        expect(true).toBe(result.changed);
+        expect(false).toBe(result.failed);
     });
-
-    expect(true).toBe(result.changed);
-    expect(false).toBe(result.failed);
-}, 60 * 1000);
-
-it('should upload file content from string', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        content: 'Hello world!',
-        remotePath: '/tmp/test-file3',
-    });
-
-    expect(result.changed).toBe(true);
-    expect(result.failed).toBe(false);
-
-    const catResult = await host.command({
-        command: 'cat /tmp/test-file3',
-    });
-
-    expect(catResult.code).toBe(0);
-    expect(catResult.stdout).toBe('Hello world!');
-
-}, 60 * 1000);
-
-it('should replace file if it already exists but has been modified', async () => {
-    const host = createHost();
-    await host.command({command: 'echo 1 > /tmp/modified_file'});
-
-    const result = await file.upload(host, {
-        localPath: __dirname + '/../../.gitignore',
-        remotePath: '/tmp/modified_file',
-    });
-
-    expect(true).toBe(result.changed);
-    expect(false).toBe(result.failed);
+    
+    it('should return "changed: false" if the file was already uploaded from a local file', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            localPath: __dirname + '/../../.gitignore',
+            remotePath: '/tmp/already_uploaded_file',
+        });
+    
+        expect(true).toBe(result.changed);
+        expect(false).toBe(result.failed);
+    
+        const result2 = await file.upload(host, {
+            localPath: __dirname + '/../../.gitignore',
+            remotePath: '/tmp/already_uploaded_file',
+        });
+    
+        expect(false).toBe(result2.changed);
+        expect(false).toBe(result2.failed);
+    }, 60 * 1000);
+    
+    it('should return "changed: false" if the file was already uploaded from a string', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            content: 'Hello world!',
+            remotePath: '/tmp/already_uploaded_file2',
+        });
+    
+        expect(result.changed).toBe(true);
+        expect(result.failed).toBe(false);
+    
+        const result2 = await file.upload(host, {
+            content: 'Hello world!',
+            remotePath: '/tmp/already_uploaded_file2',
+        });
+    
+        expect(result2.changed).toBe(false);
+        expect(result2.failed).toBe(false);
+    }, 60 * 1000);
+    
+    it('should return an error when uploading a non-existent file', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            localPath: __dirname + '/non-existent-file',
+            remotePath: '/tmp/non-existent-file',
+        });
+    
+        expect(true).toBe(result.failed);
+        expect('local file does not exist').toBe(result.error);
+    }, 60 * 1000);
+    
+    it('should return an error when uploading to a non-existent directory', async () => {
+        const host = createHost();
+        const result = await file.upload(host, {
+            localPath: __dirname + '/../../.gitignore',
+            remotePath: '/tmp/non-existent-directory/.gitignore',
+        });
+    
+        expect(true).toBe(result.failed);
+        expect('remote directory does not exist').toBe(result.error);
+    }, 60 * 1000);    
 });
-
-it('should return "changed: false" if the file was already uploaded from a local file', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        localPath: __dirname + '/../../.gitignore',
-        remotePath: '/tmp/already_uploaded_file',
-    });
-
-    expect(true).toBe(result.changed);
-    expect(false).toBe(result.failed);
-
-    const result2 = await file.upload(host, {
-        localPath: __dirname + '/../../.gitignore',
-        remotePath: '/tmp/already_uploaded_file',
-    });
-
-    expect(false).toBe(result2.changed);
-    expect(false).toBe(result2.failed);
-}, 60 * 1000);
-
-it('should return "changed: false" if the file was already uploaded from a string', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        content: 'Hello world!',
-        remotePath: '/tmp/already_uploaded_file2',
-    });
-
-    expect(result.changed).toBe(true);
-    expect(result.failed).toBe(false);
-
-    const result2 = await file.upload(host, {
-        content: 'Hello world!',
-        remotePath: '/tmp/already_uploaded_file2',
-    });
-
-    expect(result2.changed).toBe(false);
-    expect(result2.failed).toBe(false);
-}, 60 * 1000);
-
-it('should return an error when uploading a non-existent file', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        localPath: __dirname + '/non-existent-file',
-        remotePath: '/tmp/non-existent-file',
-    });
-
-    expect(true).toBe(result.failed);
-    expect('local file does not exist').toBe(result.error);
-}, 60 * 1000);
-
-it('should return an error when uploading to a non-existent directory', async () => {
-    const host = createHost();
-    const result = await file.upload(host, {
-        localPath: __dirname + '/../../.gitignore',
-        remotePath: '/tmp/non-existent-directory/.gitignore',
-    });
-
-    expect(true).toBe(result.failed);
-    expect('remote directory does not exist').toBe(result.error);
-}, 60 * 1000);
 
 describe('createDirectory method', () => {
     it('should successfully create directory', async () => {
